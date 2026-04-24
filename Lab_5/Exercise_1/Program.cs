2ccca075404198d07d70a325c422187f7b25a690
mohammad-mu <mohammadaly.mu@hotmail.com>
2026-04-24 14:55:09 +0300

    Create Program.cs

---
+++ Lab_5/Exercise_1/Program.cs
@@ -0,0 +1,31 @@
+using System;
+class Book 
+{
+    public string Title;
+    public string Author;
+    public Book(string title, string author) 
+    {
+        Title = title;
+        Author = author;
+    }
+    public void PrintDetails() 
+    {
+        Console.WriteLine($"Title: {Title}, Author: {Author}");
+    }
+}
+class Program 
+{
+    static void Main() 
+    {
+        Book[] library = new Book[3];
+        library[0] = new Book("C# Basics", "Mohammad Handosa");
+        library[1] = new Book("OOP Fundamentals", "Sara El-Metwally");
+        library[2] = new Book("Logic Circuits", "Muhammed Mahfouz");
+        Console.WriteLine("--- Book List ---");
+        foreach (Book b in library) 
+        {
+            if (b != null) 
+                b.PrintDetails();
+        }
+    }
+}
